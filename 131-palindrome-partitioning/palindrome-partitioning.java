60cc0556b75c5c02505467bc78de9830e13ddc80
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2024-06-05 02:16:58 +0530

    Time: 7 ms (96.20%) | Memory: 56.7 MB (68.54%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -7,19 +7,10 @@ class Solution {
         return result;
     }
 
-    void dfs(
-        List<List<String>> result,
-        String s,
-        int start,
-        List<String> currentList,
-        boolean[][] dp
-    ) {
+    void dfs(List<List<String>> result, String s, int start, List<String> currentList, boolean[][] dp) {
         if (start >= s.length()) result.add(new ArrayList<>(currentList));
         for (int end = start; end < s.length(); end++) {
-            if (
-                s.charAt(start) == s.charAt(end) &&
-                (end - start <= 2 || dp[start + 1][end - 1])
-            ) {
+            if (s.charAt(start) == s.charAt(end) && (end - start <= 2 || dp[start + 1][end - 1])) {
                 dp[start][end] = true;
                 currentList.add(s.substring(start, end + 1));
                 dfs(result, s, end + 1, currentList, dp);
