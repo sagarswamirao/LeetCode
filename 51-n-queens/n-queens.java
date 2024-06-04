45217c7280d8872c6185cf3c68edf9c45e59ccc7
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2024-06-04 08:33:07 +0530

    Time: 3 ms (59.07%) | Memory: 44.5 MB (80.72%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -2,11 +2,11 @@ class Solution {
     public List<List<String>> solveNQueens(int n) {
         int[][] board=new int[n][n];
         List<List<String>> boardAns=new ArrayList<>();
-        compute(board, 0, n, boardAns);
+        compute(board, 0, n, boardAns, new int[n],new int[(2*n -1)],new int[(2*n -1)]);
         return boardAns;
     }
 
-    public void compute(int[][] board, int i, int n, List<List<String>> boardAns){
+    public void compute(int[][] board, int i, int n, List<List<String>> boardAns, int[] columnHash, int[] urHash, int[] ulHash){
         if(i==n){
            List<String> tempS=new ArrayList<>();
             for(int bi=0;bi<n;bi++){
@@ -24,23 +24,23 @@ class Solution {
             return;
         }
         for(int tj=0;tj<n;tj++){
-            if(validate(board, i, tj, n, n)){
+            if(validate(board, i, tj, n, n, columnHash, urHash, ulHash)){
                 board[i][tj]=1;
-                compute(board, i+1, n, boardAns);
+                compute(board, i+1, n, boardAns, columnHash, urHash, ulHash);
                 board[i][tj]=0;
             }
         }
     }
-    public boolean validate(int[][] board, int i, int j, int row, int column){
+    public boolean validate(int[][] board, int i, int j, int row, int column, int[] columnHash, int[] urHash, int[] ulHash){
         //move diagonally top and bottom
         //move vertically and horizontally
 
         //COLUMNS of the row
-        for(int temp_j=0;temp_j<column;temp_j++){
-            if(board[i][temp_j]==1){
-                return false;
-            }
-        }
+        // for(int temp_j=0;temp_j<column;temp_j++){
+        //     if(board[i][temp_j]==1){
+        //         return false;
+        //     }
+        // }
         // ROWS of the column
         for(int temp_i=0;temp_i<i;temp_i++){
             if(board[temp_i][j]==1){
@@ -68,32 +68,105 @@ class Solution {
             x_i--;
             x_j++;
         }
-
-        // NO NEED TO USE IT AS WE ARE GOING ROW BY ROW, AND WE UNDERSTAND THAT LOWER LEVELS WON'T YET HAVE ANY QUEEN
-        //BOTTOM RIGHT
-        // x_i=i;
-        // x_j=j;
-        // while(x_i<row && x_j<column){
-        //     if(board[x_i][x_j]==1){
-        //         return false;
-        //     }
-        //     x_i++;
-        //     x_j++;
-        // }
-
-        // BOTTOM LEFT 
-        // x_i=i;
-        // x_j=j;
-        // while(x_i<row && x_j>=0){
-        //     if(board[x_i][x_j]==1){
-        //         return false;
-        //     }
-        //     x_i++;
-        //     x_j--;
-        // }
-
-
-
         return true;
     }
 }
+// class Solution {
+//     public List<List<String>> solveNQueens(int n) {
+//         int[][] board=new int[n][n];
+//         List<List<String>> boardAns=new ArrayList<>();
+//         compute(board, 0, n, boardAns);
+//         return boardAns;
+//     }
+
+//     public void compute(int[][] board, int i, int n, List<List<String>> boardAns){
+//         if(i==n){
+//            List<String> tempS=new ArrayList<>();
+//             for(int bi=0;bi<n;bi++){
+//                 StringBuilder sb=new StringBuilder();
+//                 for(int bj=0;bj<n;bj++){
+//                     if(board[bi][bj]==1){
+//                         sb.append("Q");
+//                     }else{
+//                         sb.append(".");
+//                     }
+//                 }
+//                 tempS.add(sb.toString());          
+//             }
+//             boardAns.add(tempS);
+//             return;
+//         }
+//         for(int tj=0;tj<n;tj++){
+//             if(validate(board, i, tj, n, n)){
+//                 board[i][tj]=1;
+//                 compute(board, i+1, n, boardAns);
+//                 board[i][tj]=0;
+//             }
+//         }
+//     }
+//     public boolean validate(int[][] board, int i, int j, int row, int column){
+//         //move diagonally top and bottom
+//         //move vertically and horizontally
+
+//         //COLUMNS of the row
+//         for(int temp_j=0;temp_j<column;temp_j++){
+//             if(board[i][temp_j]==1){
+//                 return false;
+//             }
+//         }
+//         // ROWS of the column
+//         for(int temp_i=0;temp_i<i;temp_i++){
+//             if(board[temp_i][j]==1){
+//                 return false;
+//             }
+//         }
+
+//         // DIAGONAL UP LEFT
+//         int x_i=i,x_j=j;
+//         while(x_i>=0 && x_j>=0){
+//             if(board[x_i][x_j]==1){
+//                 return false;
+//             }
+//             x_i--;
+//             x_j--;
+//         }
+
+//         // DIAGONAL UP RIGHT
+//         x_i=i;
+//         x_j=j;
+//         while(x_i>=0 && x_j<column){
+//             if(board[x_i][x_j]==1){
+//                 return false;
+//             }
+//             x_i--;
+//             x_j++;
+//         }
+
+//         // NO NEED TO USE IT AS WE ARE GOING ROW BY ROW, AND WE UNDERSTAND THAT LOWER LEVELS WON'T YET HAVE ANY QUEEN
+//         //BOTTOM RIGHT
+//         // x_i=i;
+//         // x_j=j;
+//         // while(x_i<row && x_j<column){
+//         //     if(board[x_i][x_j]==1){
+//         //         return false;
+//         //     }
+//         //     x_i++;
+//         //     x_j++;
+//         // }
+
+//         // BOTTOM LEFT 
+//         // x_i=i;
+//         // x_j=j;
+//         // while(x_i<row && x_j>=0){
+//         //     if(board[x_i][x_j]==1){
+//         //         return false;
+//         //     }
+//         //     x_i++;
+//         //     x_j--;
+//         // }
+
+
+
+//         return true;
+//     }
+// }
