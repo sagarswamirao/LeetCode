617192889f53a0ffa1309167ad1a7006ef1269e8
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2024-06-05 09:52:33 +0530

    Time: 588 ms (25.68%) | Memory: 94.2 MB (5.02%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.java
+++ 60-permutation-sequence/permutation-sequence.java
@@ -1,22 +1,18 @@
 class Solution {
     public String getPermutation(int n, int k) {
         char[] nums = new char[n];
-        List<List<Character>> ans = new ArrayList<>();
+        List<String> ans = new ArrayList<>();
         for(int i=1;i<=n;i++){
             nums[i-1]=(char)(i + '0');
         }
 
-        compute(0, nums, new ArrayList<>(), ans, new boolean[n], k);
-        StringBuilder sb=new StringBuilder();
-        for(int i=0;i<n;i++){
-            sb.append(ans.getLast().get(i).toString());
-        }
-        return sb.toString();
+        compute(0, nums, new StringBuilder(), ans, new boolean[n], k);
+        return ans.getLast();
     }
 
-    public boolean compute(int index, char[] nums, ArrayList<Character> dp, List<List<Character>> ans, boolean[] taken, int k){
+    public boolean compute(int index, char[] nums, StringBuilder dp, List<String> ans, boolean[] taken, int k){
         if(index==nums.length){
-            ans.add(new ArrayList<>(dp));
+            ans.add(dp.toString());
             if(k==ans.size()){
                 return true;
             }
@@ -25,13 +21,13 @@ class Solution {
 
         for(int i=0;i<nums.length;i++){
             if(taken[i]==true) continue;
-            dp.add(nums[i]);
+            dp.append(nums[i]);
             taken[i]=true;
             if(compute(index+1, nums, dp, ans, taken, k)){
                 return true;
             }
             taken[i]=false;
-            dp.removeLast();
+            dp.deleteCharAt(dp.length()-1);
         }
 
         return false;
