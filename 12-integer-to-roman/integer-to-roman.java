e957660479397484b2ac2eb33bf6737cbb0d0888
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2024-06-06 09:19:22 +0530

    Time: 9 ms (23.41%) | Memory: 44.9 MB (12.07%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String intToRoman(int num) {
+        StringBuilder sb=new StringBuilder();
+        HashMap<Integer, String> romanIntMap=new LinkedHashMap<>();
+        romanIntMap.put(1000,"M");
+        romanIntMap.put(900,"CM");
+        romanIntMap.put(500,"D");
+        romanIntMap.put(400,"CD");
+        romanIntMap.put(100,"C");
+        romanIntMap.put(90,"XC");
+        romanIntMap.put(50,"L");
+        romanIntMap.put(40,"XL");
+        romanIntMap.put(10,"X");
+        romanIntMap.put(9,"IX");
+        romanIntMap.put(5,"V");
+        romanIntMap.put(4,"IV");
+        romanIntMap.put(1,"I");
+
+        //goal is to start from the highest denomination
+        for(Map.Entry<Integer, String> entry: romanIntMap.entrySet()){
+            while (entry.getKey() <= num) {
+                num = num - entry.getKey();
+                sb.append(entry.getValue());
+            } 
+        }
+        return sb.toString();
+    }
+
+}
