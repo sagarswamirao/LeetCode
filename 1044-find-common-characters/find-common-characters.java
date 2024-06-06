23c70c4a027de42301b0c74a353b126be4666f92
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2024-06-06 06:02:38 +0530

    Time: 3 ms (88.56%) | Memory: 42.8 MB (73.15%) - LeetSync

--- 1044-find-common-characters/find-common-characters.java
+++ 1044-find-common-characters/find-common-characters.java
@@ -3,20 +3,22 @@ class Solution {
         int[] minFreq = new int[26];
         Arrays.fill(minFreq, Integer.MAX_VALUE);
         
+        int[] freq = new int[26];
         for (String word : words) {
-            int[] freq = new int[26];
             for (char c : word.toCharArray()) {
                 freq[c - 'a']++;
             }
             for (int i = 0; i < 26; i++) {
                 minFreq[i] = Math.min(minFreq[i], freq[i]);
+                freq[i]=0;
             }
         }
         
         List<String> result = new ArrayList<>();
         for (int i = 0; i < 26; i++) {
-            while (minFreq[i]-- > 0) {
+            while (minFreq[i] > 0) {
                 result.add(String.valueOf((char) (i + 'a')));
+                minFreq[i]--;
             }
         }
         return result;
