a5590e69fa6d8dee7a6abec6158241b3b76cb24a
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2024-06-08 23:31:35 +0530

    Time: 6 ms (19.00%) | Memory: 41 MB (5.35%) - LeetSync

--- 397-integer-replacement/integer-replacement.java
+++ 397-integer-replacement/integer-replacement.java
@@ -14,14 +14,14 @@ class Solution {
         }
         int a=Integer.MAX_VALUE,b=Integer.MAX_VALUE;
         if(n%2==0){
-            a= recursion(n/2, curr_steps+1);
+            return recursion(n/2, curr_steps+1);
         }else{
-            if(n>0){
-                b = Math.min(recursion(n-1, curr_steps+1), recursion(n+1, curr_steps+1));
-            }else{         
-                b = recursion(n+1, curr_steps+1);
-            }
+            // if(n>0){
+                return Math.min(recursion(n-1, curr_steps+1), recursion(n+1, curr_steps+1));
+            // }else{         
+            //     return recursion(n+1, curr_steps+1);
+            // }
         }
-        return Math.min(a, b);
+        // return Math.min(a, b);
     }
 }
