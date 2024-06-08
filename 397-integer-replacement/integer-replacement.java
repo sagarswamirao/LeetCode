d705272fca0f0ff273edea35752849bf51c53f2b
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2024-06-08 23:30:51 +0530

    Time: 7 ms (7.25%) | Memory: 40.3 MB (37.65%) - LeetSync

--- 397-integer-replacement/integer-replacement.java
+++ 397-integer-replacement/integer-replacement.java
@@ -3,6 +3,11 @@ class Solution {
         return recursion(n, 0);
     }
 
+    // For int: 2^31 - 1
+    // For long: 2^63 - 1
+    // For float: 2^128
+    // For double: 2^1024
+    // Although the constraint in 2^31 -1, but as we are doing n+1, I code goes for a toss
     public int recursion(long n, int curr_steps){
         if(n==1){
             return curr_steps;
