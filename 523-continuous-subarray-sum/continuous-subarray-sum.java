310f58be434bd43b825b97efb7211d1e757dfdce
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2024-06-08 13:04:15 +0530

    Time: 23 ms (56.12%) | Memory: 56.5 MB (99.83%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int prefixMod = 0;
+        HashMap<Integer, Integer> modSeen = new HashMap<>();
+        modSeen.put(0, -1);
+
+        for (int i = 0; i < nums.length; i++) {
+            prefixMod = (prefixMod + nums[i]) % k; 
+// for [23,2,4,6,7] and k=6, this is the mod values 5,1,5,5,1; So if 5 & 5, then numbers exist, 1 to 1 is a sub array and so is 5 to 5
+            if (modSeen.containsKey(prefixMod)) {
+                // ensures that the size of subarray is at least 2
+                if (i - modSeen.get(prefixMod) > 1) {
+                    return true;
+                }
+            } else {
+                // mark the value of prefixMod with the current index.
+                modSeen.put(prefixMod, i);
+            }
+        }
+
+        return false;
+    }
+}
