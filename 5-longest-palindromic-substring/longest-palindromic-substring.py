e9ad29cffecfe48abdf3dd24cc35ce0278b2096c
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2024-08-08 03:22:05 +0530

    Time: 3801 ms (23.36%) | Memory: 16.5 MB (67.29%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -16,6 +16,6 @@ class Solution:
         for end in range(len(s), 0, -1):
             for start in range(len(s) - end + 1):
                 if check(start, start + end):
-                    return s[start : start + end]
+                    return s[start:start + end]
 
         return ""
