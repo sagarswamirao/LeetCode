46b9d9aaa3204b37ea6750c308fabbb435bfaf93
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2024-09-23 14:29:38 -0600

    Time: 1 ms (91.15%), Space: 47.8 MB (46.39%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,26 +1,22 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        boolean[][] visited=new boolean[isConnected.length][isConnected.length];
+        boolean[] visited=new boolean[isConnected.length];
         int count=0;
         for(int i=0;i<isConnected.length;i++){
-            for(int j=0;j<isConnected.length;j++){
-                if(isConnected[i][j]==1 && !visited[i][j]){
+                if(!visited[i]){
                     count+=1;
-                    // visited[i][j]=true;
-                    // visited[j][i]=true;
                     callRecursive(isConnected, visited, i);       
                 }
-            }
+            // }
         }
         return count;
     }
 
-    public void callRecursive(int[][] isConnected, boolean[][] visited, int i){
-        for(int j=0;j<isConnected.length;j++){
-            if(isConnected[i][j]==1 && !visited[i][j]){
-                visited[i][j]=true;
-                visited[j][i]=true;
-                callRecursive(isConnected, visited, j);
+    public void callRecursive(int[][] isConnected, boolean[] visited, int currNode){
+        visited[currNode]=true;
+        for(int neighbor=0;neighbor<isConnected.length;neighbor++){
+            if(isConnected[currNode][neighbor]==1 && !visited[neighbor]){
+                callRecursive(isConnected, visited, neighbor);
             }
         }
 
