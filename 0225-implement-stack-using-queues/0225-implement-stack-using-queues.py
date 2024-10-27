62e980d44d0398d5362b26b173e58e8b0c065ad3
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2024-10-26 21:36:10 -0600

    Time: 0 ms (100%), Space: 16.7 MB (20.76%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -1,42 +1,42 @@
 from collections import deque
 class MyStack:
-
-    def __init__(self):
-        self.d1=deque() #insert
-        self.d2=deque() #poll
-        self.top_val=-1
-
-    def push(self, x: int) -> None:
-        while self.d2:
-            popEle=d2.popleft()
-            self.d1.append(popEle)
-        self.d1.append(x)
-        self.top_val=x
 #  q1 1 2 3 4 5
 #  get top: 5
 #  pop
 #  q1
 #  q2 1 2 3 4
 
+    def __init__(self):
+        self.d1=deque() #insert
+        self.d2=deque() #poll
+
+    def push(self, x: int) -> None:
+        self.d1.append(x) # as we are always swapping and making sure to keep d2 empty
+
     def pop(self) -> int:
         if self.empty():
             return -1
         popEle=None
-        while self.d1:
-            if len(self.d1)==1:
-                popEle=self.d1.popleft()
-                break
+        while len(self.d1)>1:
             self.d2.append(self.d1.popleft())
-        self.top_val=-1 if not self.d2 else self.d2[-1]
+        popEle=self.d1.popleft()
         self.d1, self.d2= self.d2, self.d1
         return popEle
             
     def top(self) -> int:
-        return self.top_val
+        if self.empty():
+            return -1
+        topEle=None
+        while len(self.d1)>1:
+            self.d2.append(self.d1.popleft())
+        topEle=self.d1.popleft()
+        self.d2.append(topEle)
+        self.d1, self.d2= self.d2, self.d1
+        return topEle
+
 
     def empty(self) -> bool:
-        if not self.d1 and not self.d2:
-            self.top_val=-1
+        if len(self.d1)==0: # as we are always swapping and making sure to keep d2 empty
             return True   
         return False
 
