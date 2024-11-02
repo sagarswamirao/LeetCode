2d4be342fb0a5948ab0e88fd8078de0f8d59002c
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2024-11-02 06:09:03 -0600

    Time: 34 ms (35.95%), Space: 55.1 MB (25.03%) - LeetHub

---
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -0,0 +1,36 @@
+class Point{
+    public double dist;
+    public int index;
+
+    Point(double dist, int index){
+        this.dist=dist;
+        this.index=index;
+    }
+}
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        //k is the heap size; k-closest means k-lowest, so max-heap will work
+        PriorityQueue<Point> maxHeap=new PriorityQueue<>((a,b)->(int) (b.dist-a.dist));
+        for(int i=0;i<points.length;i++){
+            maxHeap.add(new Point(getEuclideanDistance(points[i][0], points[i][1]), i));
+            if(maxHeap.size()>k){
+                maxHeap.poll();
+            }
+        }
+
+        int[][] ans=new int[maxHeap.size()][2];
+        int i=0;
+        while(!maxHeap.isEmpty()){
+            Point popItem=maxHeap.poll();
+            ans[i]=points[popItem.index];
+            i++;
+        }
+        return ans;
+    }
+
+    public double getEuclideanDistance(int x2, int y2){
+        int x1=0;
+        int y1=0;
+        return (int) Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2);
+    }
+}
