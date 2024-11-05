4581fb76aeec84e868704cc587e17c7ea1461710
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2024-11-05 11:07:20 -0700

    Time: 6 ms (53.62%), Space: 18.1 MB (88.91%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,28 +1,25 @@
 class Solution:
-    def dfs(self, node, adj, visit, inStack):
-        # If the node is already in the stack, we have a cycle.
-        if inStack[node]:
-            return True
-        if visit[node]:
-            return False
-        # Mark the current node as visited and part of current recursion stack.
-        visit[node] = True
-        inStack[node] = True
-        for neighbor in adj[node]:
-            if self.dfs(neighbor, adj, visit, inStack):
-                return True
-        # Remove the node from the stack.
-        inStack[node] = False
-        return False
-
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj = [[] for _ in range(numCourses)]
-        for prerequisite in prerequisites:
-            adj[prerequisite[1]].append(prerequisite[0])
-
-        visit = [False] * numCourses
-        inStack = [False] * numCourses
-        for i in range(numCourses):
-            if self.dfs(i, adj, visit, inStack):
+        preq_map={}
+        for course, preq in prerequisites:
+            if course not in preq_map:
+                preq_map[course]=[]
+            preq_map[course].append(preq)
+        
+        for course in preq_map.keys():
+            if self.dfs_has_cycle(preq_map, course, set()):
                 return False
         return True
+    
+    def dfs_has_cycle(self, preq_map, course, visited):
+        if course in visited:
+            return True
+        if course not in preq_map or len(preq_map[course])==0:
+            return False
+        visited.add(course)
+        for neighbor in preq_map[course]:
+            if self.dfs_has_cycle(preq_map, neighbor,visited):
+                return True
+            preq_map[course].remove(neighbor)
+        visited.remove(course)
+        return False
