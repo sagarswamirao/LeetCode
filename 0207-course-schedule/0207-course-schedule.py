d1883ec7c123fa830a4b7511cb0ecab8b629e000
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-01-21 23:49:27 +0530

    Time: 0 ms (100%), Space: 19.1 MB (40.3%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,11 +1,10 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        preq_map={}
+        preq_map={i:[] for i in range(numCourses)}
         for course, preq in prerequisites:
-            if course not in preq_map:
-                preq_map[course]=[]
             preq_map[course].append(preq)
         
+        #check for all the courses recursively and even if one course has a cycle, return False
         for course in preq_map.keys():
             if self.dfs_has_cycle(preq_map, course, set()):
                 return False
@@ -14,7 +13,7 @@ class Solution:
     def dfs_has_cycle(self, preq_map, course, visited):
         if course in visited:
             return True
-        if course not in preq_map or len(preq_map[course])==0:
+        if len(preq_map[course])==0:
             return False
         visited.add(course)
         for neighbor in preq_map[course]:
