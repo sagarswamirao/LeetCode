8523a3a7441289abd14c755a097ee75afa2083fd
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-01-21 22:58:57 +0530

    Time: 47 ms (69.05%), Space: 34.4 MB (25.34%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -3,16 +3,14 @@ class Solution:
         hset=set(nums)
         max_seq_length=0
 
-        for num in nums:           
+        for num in hset:           
             if num-1 not in hset:
                 curr_seq_element=num
-                curr_seq_length=0
-                while curr_seq_element in hset:
-                    hset.remove(curr_seq_element)
+                curr_seq_length=1
+
+                while curr_seq_element+1 in hset:
                     curr_seq_element+=1
                     curr_seq_length+=1
                 max_seq_length=max(max_seq_length, curr_seq_length)
-            else:
-                continue
 
         return max_seq_length
