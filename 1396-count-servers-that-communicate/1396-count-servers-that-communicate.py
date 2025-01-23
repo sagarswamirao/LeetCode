b5d3206c94675a79cba2ab241e3fd1899609b1ec
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-01-23 06:56:00 +0530

    Time: 15 ms (73.78%), Space: 19.3 MB (80.9%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.py
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.py
@@ -5,15 +5,15 @@ class Solution:
 
         rows=len(grid)
         cols=len(grid[0])
-        row_counts = [0] * len(grid[0])
-        col_counts = [0] * len(grid)
+        row_counts = [0] * len(grid)
+        col_counts = [0] * len(grid[0])
 
         # Count servers in each row and each column
         for row in range(len(grid)):
             for col in range(len(grid[0])):
                 if grid[row][col]:
-                    row_counts[col] += 1
-                    col_counts[row] += 1
+                    row_counts[row] += 1
+                    col_counts[col] += 1
         
         communicable_servers_count = 0
 
@@ -21,7 +21,7 @@ class Solution:
         for row in range(len(grid)):
             for col in range(len(grid[0])):
                 if grid[row][col]:
-                    if row_counts[col] > 1 or col_counts[row] > 1:
+                    if row_counts[row] > 1 or col_counts[col] > 1:
                         communicable_servers_count += 1
 
         return communicable_servers_count
