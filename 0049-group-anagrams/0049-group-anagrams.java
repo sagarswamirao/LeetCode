39f3df2a506875a9417c6e2dc26aeeee3d255109
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-01-28 13:44:56 -0700

    Time: 18 ms (24.87%), Space: 48.6 MB (17.51%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,16 +1,37 @@
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+        
+//         HashMap<String, List<String>> hashy = new HashMap<>();
+//         for(String s: strs){
+//             char[] arr = s.toCharArray();
+//             Arrays.sort(arr);
+//             String key = new String(arr);
+//             if(!hashy.containsKey(key)){
+//                 hashy.put(key, new ArrayList<String>());
+//             }
+//             hashy.get(key).add(s);
+//         }
+//         return new ArrayList<>(hashy.values());
+//     }
+// }
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        
-        HashMap<String, List<String>> hashy = new HashMap<>();
-        for(String s: strs){
-            char[] arr = s.toCharArray();
-            Arrays.sort(arr);
-            String key = new String(arr);
-            if(!hashy.containsKey(key)){
-                hashy.put(key, new ArrayList<String>());
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        int[] count = new int[26];
+        for (String s : strs) {
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray()) count[c - 'a']++;
+
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < 26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
             }
-            hashy.get(key).add(s);
+            String key = sb.toString();
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
         }
-        return new ArrayList<>(hashy.values());
+        return new ArrayList(ans.values());
     }
 }
