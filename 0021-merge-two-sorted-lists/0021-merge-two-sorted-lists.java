d063db7c513ed1537143ad0fdfbb23c5167dcc3d
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-01-28 14:13:52 -0700

    Time: 0 ms (100%), Space: 42.4 MB (86.01%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -34,40 +34,5 @@ class Solution {
         }
         return l3head.next;
     }
-    // public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-    //   ListNode l1_head=list1;
-    //   ListNode l2_head=list2;
-    //   ListNode l3_tail=new ListNode(0);
-    //   ListNode l3_head=l3_tail;
-    //   while(l1_head!=null || l2_head!=null){
-    //       if(l1_head!=null && l2_head!=null){
-    //       if(l1_head.val>l2_head.val){
-    //           l3_tail.next=new ListNode(l2_head.val);
-    //           l3_tail=l3_tail.next;
-    //           l2_head=l2_head.next;
-    //       }else if(l1_head.val < l2_head.val){
-    //           l3_tail.next=new ListNode(l1_head.val);
-    //           l3_tail=l3_tail.next;
-    //           l1_head=l1_head.next;
-    //       }else{
-    //           l3_tail.next=new ListNode(l1_head.val);
-    //           l3_tail=l3_tail.next;
-    //           l3_tail.next=new ListNode(l2_head.val);
-    //           l3_tail=l3_tail.next;
-    //           l1_head=l1_head.next;
-    //           l2_head=l2_head.next;
-    //       }
-    //     }else if(l1_head!=null){
-    //         l3_tail.next=new ListNode(l1_head.val);
-    //         l3_tail=l3_tail.next;
-    //         l1_head=l1_head.next;
-    //     }else if(l2_head!=null){
-    //         l3_tail.next=new ListNode(l2_head.val);
-    //         l3_tail=l3_tail.next;
-    //         l2_head=l2_head.next;
-    //     }
-
-    //   }  
-    //   return l3_head.next;
-    // }
+ 
 }
