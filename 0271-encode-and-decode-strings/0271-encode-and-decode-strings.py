5eea07cd79237a2e7a90470d9dba3549878152e9
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-01-28 12:59:34 -0700

    Time: 75 ms (36.18%), Space: 18 MB (73.02%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -20,10 +20,10 @@ class Codec:
             while s[j]!='#': #getting the length of next string to read
                 j+=1
             
-            length_of_string=int(s[i:j])
-
-            res.append(s[j+1: j+1+length_of_string])
-
+            length_of_string=int(s[i:j]) #parse that string to int, to get the value
+            # j is #, so we want the string starting after it, until the length_of_string
+            res.append(s[j+1: j+1+length_of_string]) 
+            # we have extracted till j+1, str_length, now the next length
             i = j + 1 + length_of_string
         
         return res
