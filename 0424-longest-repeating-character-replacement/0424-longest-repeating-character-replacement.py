fda957021f15bbadac907692e2eb79aeadb7995b
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-01-30 15:11:17 -0700

    Time: 199 ms (20.86%), Space: 18 MB (37.19%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -9,11 +9,10 @@ class Solution:
 
         l=0
         freq=[0]*26
-        for r in range(len(s)): #for each index in string
-            freq[getIndexValueChar(s[r])]+=1 #increment the frequency of the character 's' points to
-            max_freq=max(max_freq, freq[getIndexValueChar(s[r])]) #find the max frequency
-            # curr_exceptions=  #between the range [l,r], we know the max_frequency. so we can compute the exceptions    
-            while ((r-l+1) - max_freq) >max_exceptions: #
+        for r in range(len(s)):
+            freq[getIndexValueChar(s[r])]+=1
+            max_freq=max(max_freq, freq[getIndexValueChar(s[r])])  
+            while ((r-l+1) - max_freq) >max_exceptions:
                 freq[getIndexValueChar(s[l])]-=1
                 max_freq=max(freq)
                 l+=1
