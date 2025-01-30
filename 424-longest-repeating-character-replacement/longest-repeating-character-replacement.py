6e86802308c7b6b83ea4100a6df1d1bf2a40f254
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-01-30 14:08:51 -0700

    Time: 171 ms (37.53%) | Memory: 18.2 MB (17.62%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,25 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        max_exceptions=k
+        l=0
+        length=1
+        max_freq=0
+        freq=[0]*26
+
+        def getIndexValueChar(ch):
+            return ord(ch) - ord('A')
+        for r in range(len(s)):
+            freq[getIndexValueChar(s[r])]+=1
+            max_freq=max(max_freq, freq[getIndexValueChar(s[r])])
+            curr_exceptions= (r-l+1) - max_freq     
+            if curr_exceptions>max_exceptions:
+                freq[getIndexValueChar(s[l])]-=1
+                curr_exceptions=(r-l+1) - max(freq)
+                l+=1
+
+            length=max(length, r-l+1)
+
+        return length
+            
+
+
