7ffb4048446d66a5436158381e6667e4f7e1ec07
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-01-30 15:09:03 -0700

    Time: 197 ms (21%), Space: 18.2 MB (17.62%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,22 +1,21 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         max_exceptions=k
-        l=0
         length=1
         max_freq=0
-        freq=[0]*26
 
-        @cache
         def getIndexValueChar(ch):
             return ord(ch) - ord('A')
 
-        for r in range(len(s)):
-            freq[getIndexValueChar(s[r])]+=1
-            max_freq=max(max_freq, freq[getIndexValueChar(s[r])])
-            curr_exceptions= (r-l+1) - max_freq     
-            if curr_exceptions>max_exceptions:
+        l=0
+        freq=[0]*26
+        for r in range(len(s)): #for each index in string
+            freq[getIndexValueChar(s[r])]+=1 #increment the frequency of the character 's' points to
+            max_freq=max(max_freq, freq[getIndexValueChar(s[r])]) #find the max frequency
+            # curr_exceptions=  #between the range [l,r], we know the max_frequency. so we can compute the exceptions    
+            while ((r-l+1) - max_freq) >max_exceptions: #
                 freq[getIndexValueChar(s[l])]-=1
-                curr_exceptions=(r-l+1) - max(freq)
+                max_freq=max(freq)
                 l+=1
 
             length=max(length, r-l+1)
