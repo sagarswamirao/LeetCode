dbe4864870c4d9f8bf8fcfa4533407c95911f95d
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-01-30 14:08:46 -0700

    Time: 171 ms (37.53%), Space: 18.2 MB (17.62%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -5,12 +5,15 @@ class Solution:
         length=1
         max_freq=0
         freq=[0]*26
+
+        def getIndexValueChar(ch):
+            return ord(ch) - ord('A')
         for r in range(len(s)):
-            freq[ord(s[r])-ord('A')]+=1
-            max_freq=max(max_freq, freq[ord(s[r])-ord('A')])
+            freq[getIndexValueChar(s[r])]+=1
+            max_freq=max(max_freq, freq[getIndexValueChar(s[r])])
             curr_exceptions= (r-l+1) - max_freq     
             if curr_exceptions>max_exceptions:
-                freq[ord(s[l])-ord('A')]-=1
+                freq[getIndexValueChar(s[l])]-=1
                 curr_exceptions=(r-l+1) - max(freq)
                 l+=1
 
