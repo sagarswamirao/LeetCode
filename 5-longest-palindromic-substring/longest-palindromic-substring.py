1c56fbfdad9b54af9a3e1b8289db6dfd79f4e265
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-01-30 17:57:39 -0700

    Time: 320 ms (53.75%) | Memory: 17.9 MB (51.99%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,22 +1,50 @@
+# class Solution:
+#     def longestPalindrome(self, s: str) -> str:
+#         @cache
+#         def check(i, j):
+#             left = i
+#             right = j - 1
+
+#             while left < right:
+#                 if s[left] != s[right]:
+#                     return False
+
+#                 left += 1
+#                 right -= 1
+
+#             return True
+
+#         for end in range(len(s), 0, -1):
+#             for start in range(0, len(s) - end + 1):
+#                 if check(start, start + end):
+#                     return s[start:start + end]
+
+#         return ""
+
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        @cache
-        def check(i, j):
-            left = i
-            right = j - 1
+        ans = [0, 0]
+        res_len=0
+        left=0
+        res=''
 
-            while left < right:
-                if s[left] != s[right]:
-                    return False
+        for mid_index in range(len(s)):
+            # odd length palindrome
+            left, right =mid_index,mid_index
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if (right -left + 1) > res_len:
+                    res=s[left : right+1]
+                    res_len=(right -left + 1)
+                left -= 1
+                right += 1
 
-                left += 1
-                right -= 1
+            # even length palindrome
+            left, right=mid_index, mid_index+1
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if (right -left + 1) > res_len:
+                    res=s[left: right+1]
+                    res_len=(right -left + 1)
+                left -= 1
+                right += 1
+        return res
 
-            return True
-
-        for end in range(len(s), 0, -1):
-            for start in range(0, len(s) - end + 1):
-                if check(start, start + end):
-                    return s[start:start + end]
-
-        return ""
