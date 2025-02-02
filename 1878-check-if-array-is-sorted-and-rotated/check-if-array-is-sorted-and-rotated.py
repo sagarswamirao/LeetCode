0bc1cab93a81f42c00e7f64f3fabc8d4f425619c
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-02-02 14:22:22 -0700

    Time: 3 ms (9.91%) | Memory: 17.7 MB (48.62%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -2,12 +2,11 @@ class Solution:
     def check(self, nums: List[int]) -> bool:
         pattern_breaks=0
 
-        for i in range(0,len(nums)-1):
+        for j in range(0,2*len(nums)-1):
+            i= (j%len(nums))-1
             if nums[i]<=nums[i+1]:
                 continue
             else:
                 pattern_breaks+=1
         
-        if pattern_breaks>0 and nums[0]<nums[-1]:
-            return False
-        return True if (pattern_breaks<2) else False
+        return True if (pattern_breaks<=2) else False
