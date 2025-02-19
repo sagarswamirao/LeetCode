fff7de7a4d79c06394d28022fd2d1fc48a27ef21
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-02-18 21:45:08 -0700

    Time: 33 ms (45.95%) | Memory: 18.1 MB (42.89%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -1,25 +1,43 @@
 class Solution:
     def getHappyString(self, n: int, k: int) -> str:
-        chars=['a','b','c']
-        result_array=[]
+        if k > 3 * (2 ** (n - 1)): return ""
 
-        patterns_generated=0
-        def recursive(idx,n,ans):
-            nonlocal patterns_generated
-            if len(ans)==n:
-                patterns_generated+=1
-                if patterns_generated==k:
-                    result_array.append("".join(ans)) 
-                return
-                
-            if len(ans)>n:
-                return 
-
-            for i in range(len(chars)):
-                if i!=idx:
-                    ans.append(chars[i])
-                    recursive(i,n,ans)
-                    ans.pop()
+        happy = ['a', 'b', 'c']
+        suffix = {'a': 'bc', 'b': 'ac', 'c': 'ab'}
         
-        recursive(-1,n,[])
-        return result_array[0] if len(result_array)==1 else ""
+        for i in range(n - 1):
+            for j in range(len(happy)):
+                h = happy.pop(0)
+                s = suffix[h[-1]]
+                happy += [h + s[0], h + s[1]]
+        
+        return happy[k - 1]
+
+        
+
+
+# class Solution:
+#     def getHappyString(self, n: int, k: int) -> str:
+#         chars=['a','b','c']
+#         result_array=[]
+
+#         patterns_generated=0
+#         def recursive(idx,n,ans):
+#             nonlocal patterns_generated
+#             if len(ans)==n:
+#                 patterns_generated+=1
+#                 if patterns_generated==k:
+#                     result_array.append("".join(ans)) 
+#                 return
+                
+#             if len(ans)>n:
+#                 return 
+
+#             for i in range(len(chars)):
+#                 if i!=idx:
+#                     ans.append(chars[i])
+#                     recursive(i,n,ans)
+#                     ans.pop()
+        
+#         recursive(-1,n,[])
+#         return result_array[0] if len(result_array)==1 else ""
