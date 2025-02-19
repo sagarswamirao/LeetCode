5a59f0c87abb5c24343bedfb31339b0dadfd08b3
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-02-19 03:38:30 -0700

    Time: 0 ms (100%), Space: 17.9 MB (56.02%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -1,17 +1,28 @@
 class Solution:
     def getHappyString(self, n: int, k: int) -> str:
-        if k > 3 * (2 ** (n - 1)): return ""
+        total_happy_count = 3 * (2 ** (n - 1))
+        if k > total_happy_count: 
+            return ""
 
         happy = ['a', 'b', 'c']
-        suffix = {'a': 'bc', 'b': 'ac', 'c': 'ab'}
+        suffix = {'a': ['b', 'c'], 'b': ['a', 'c'], 'c': ['a', 'b']}
+        ans = []
         
-        for i in range(n - 1):
-            for j in range(len(happy)):
-                h = happy.pop(0)
-                s = suffix[h[-1]]
-                happy += [h + s[0], h + s[1]]
+        # Determine first character
+        partition_size = (2 ** (n - 1))  # The size of each letter's partition
+        first_idx = (k - 1) // partition_size  # 0 -> 'a', 1 -> 'b', 2 -> 'c'
+        ans.append(happy[first_idx])  # Pick the first character
+        k -= first_idx * partition_size  # Adjust k within the selected partition
         
-        return happy[k - 1]
+        # Generate the rest of the string
+        for i in range(1, n):
+            partition_size //= 2  # Reduce partition size
+            next_choices = suffix[ans[-1]]  # Get allowed next letters
+            next_idx = (k - 1) // partition_size  # Choose between the two
+            ans.append(next_choices[next_idx])  # Append the valid next character
+            k -= next_idx * partition_size  # Adjust k for the next iteration
+
+        return ''.join(ans)
 
         
 
