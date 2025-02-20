1288a90cfba27bd55b127c13c0028cfe1da46b61
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-02-20 04:33:20 -0700

    Time: 2 ms (88.01%), Space: 55.3 MB (74.72%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,16 +3,22 @@ class Solution {
         int totalProduct=1;
         int n=nums.length;
         int[] prefixProduct=new int[n];
-        int[] postfixProduct=new int[n];
+        // int[] postfixProduct=new int[n];
         prefixProduct[0]=1;
-        postfixProduct[n-1]=1;
+        // postfixProduct[n-1]=1;
         for(int i=1;i<n;i++){
             prefixProduct[i]=prefixProduct[i-1]*nums[i-1];
-            postfixProduct[n-i-1]= postfixProduct[n-i] * nums[n-i];
+            // postfixProduct[n-i-1]= postfixProduct[n-i] * nums[n-i];
         }
-        for(int i=0;i<n;i++){
-            nums[i]=prefixProduct[i]*postfixProduct[i];
+        int postfix_product=1;
+        for(int i=n-2;i>=0;i--){
+            prefixProduct[i]*=(postfix_product*nums[i+1]);
+            postfix_product=(postfix_product*nums[i+1]);
+            // postfixProduct[i]= postfixProduct[i+1] * nums[i+1];
         }
-        return nums;
+        // for(int i=0;i<n;i++){
+        //     nums[i]=prefixProduct[i]*postfixProduct[i];
+        // }
+        return prefixProduct;
     }
 }
