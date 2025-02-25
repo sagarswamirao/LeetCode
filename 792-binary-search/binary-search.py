caed5ce6cd16e0191c7b4769325827921890412d
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-02-25 15:41:26 -0700

    Time: 0 ms (100.00%) | Memory: 18.6 MB (73.72%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,23 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
+
+
+# while(left <= right) is correct. There are 2 cases:
+
+# target is between left & right. Ex: arr = 3 1 9 , target = 1
+# array has 1 element
+
+# when all the numbers are unique ==> while(left <= right)
+# when there are duplicate numbers ==> while(left < right)
