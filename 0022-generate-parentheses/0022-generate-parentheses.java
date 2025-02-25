419dd4dd8a4189594b7cf17706186dd61d26afeb
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-02-24 18:49:41 -0700

    Time: 2 ms (50.29%), Space: 43.2 MB (69.69%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,22 +1,24 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
     ArrayList<String> result= new ArrayList<String>();
-    this.recursive(new ArrayList<String>(), result, 0, n);
+    this.recursive(new ArrayList<String>(), result, 0, 0, n);
     return result;
     }
 
-    public void recursive(ArrayList<String> currString, ArrayList<String> result, int open, int n) {
-        if(open == 0 && currString.size()==2*n){
+    public void recursive(ArrayList<String> currString, ArrayList<String> result, int open, int close, int n) {
+        if(currString.size()==2*n){
             result.add(String.join("",currString));
-        }
-        if(open < 0 || open>n || currString.size()>2*n){
             return;
         }
-        currString.add("(");
-        this.recursive(currString, result, open+1, n);
-        currString.removeLast();
-        currString.add(")");
-        this.recursive(currString, result, open-1, n);
-        currString.removeLast();
-}
+        if(open<n){
+            currString.add("(");
+            this.recursive(currString, result, open+1, close, n);
+            currString.removeLast();
+        }
+        if(open > close){
+            currString.add(")");
+            this.recursive(currString, result, open, close+1, n);
+            currString.removeLast();
+        }
+    }
 }
