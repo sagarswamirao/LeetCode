bf4e341a5e744475f80d23ab7d4f026205325892
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-02-27 17:55:51 -0700

    Time: 130 ms (92.72%) | Memory: 105 MB (99.89%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.java
+++ 1023-time-based-key-value-store/time-based-key-value-store.java
@@ -1,51 +1,58 @@
-class TimeMap {
-    private Map<String, TreeMap<Integer, String>> m;
-
-    public TimeMap() {
-        m = new HashMap<>();
-    }
-
-    public void set(String key, String value, int timestamp) {
-        m.computeIfAbsent(key, k -> new TreeMap<>()).put(timestamp, value);
-    }
-
-    public String get(String key, int timestamp) {
-        if (!m.containsKey(key)) return "";
-        TreeMap<Integer, String> timestamps = m.get(key);
-        Map.Entry<Integer, String> entry = timestamps.floorEntry(timestamp);
-        return entry == null ? "" : entry.getValue();
-    }
-}
-
-//Brute Force
 // class TimeMap {
-//     private HashMap<Integer, HashMap<String, String>> hMap;
+//     private Map<String, TreeMap<Integer, String>> m;
+
 //     public TimeMap() {
-//         this.hMap=new HashMap<>();
+//         m = new HashMap<>();
 //     }
-    
+
 //     public void set(String key, String value, int timestamp) {
-//         if(!this.hMap.containsKey(timestamp)){
-//             this.hMap.put(timestamp, new HashMap<>());
-//         }
-//         this.hMap.get(timestamp).put(key,value);
+//         m.computeIfAbsent(key, k -> new TreeMap<>()).put(timestamp, value);
 //     }
-    
+
 //     public String get(String key, int timestamp) {
-//         String result="";
-
-//         if(this.hMap.size()==0){
-//             return "";
-//         }
-//         for(int curr_timestamp=timestamp;curr_timestamp>0;curr_timestamp--){
-//             if(hMap.containsKey(curr_timestamp) && hMap.get(curr_timestamp).containsKey(key)){
-//                 return hMap.get(curr_timestamp).get(key);
-//             }
-//         }
-
-//         return result;
+//         if (!m.containsKey(key)) return "";
+//         TreeMap<Integer, String> timestamps = m.get(key);
+//         Map.Entry<Integer, String> entry = timestamps.floorEntry(timestamp);
+//         return entry == null ? "" : entry.getValue();
 //     }
 // }
+
+//Brute Force
+class TimeMap {
+    private HashMap<String, HashMap<String, Integer>> hMap;
+    public TimeMap() {
+        this.hMap=new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if(!this.hMap.containsKey(key)){
+            this.hMap.put(key, new HashMap<>());
+        }
+        this.hMap.get(key).put(value,timestamp);
+    }
+    
+    public String get(String key, int timestamp) {
+
+        if(this.hMap.size()==0 || !this.hMap.containsKey(key)){
+            return "";
+        }
+        
+        int nearest_timestamp=0;
+        String nearestValue="";
+        for(Map.Entry<String, Integer> entry:this.hMap.get(key).entrySet()){
+            Integer curr_timestamp=entry.getValue();
+            String curr_value=entry.getKey();
+            if(curr_timestamp>timestamp){
+                continue;
+            }
+            if(nearest_timestamp<curr_timestamp){
+                nearestValue=curr_value;
+                nearest_timestamp=curr_timestamp;
+            }
+        }
+        return nearestValue;
+    }
+}
 
 /**
  * Your TimeMap object will be instantiated and called as such:
