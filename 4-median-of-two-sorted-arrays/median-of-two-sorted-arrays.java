4a10e2b16d8ba18a160316a36b6ad200b0c7423f
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-02-27 20:40:55 -0700

    Time: 1 ms (100.00%) | Memory: 46.4 MB (25.27%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,38 +1,35 @@
-public class Solution {
+class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] A = nums1;
-        int[] B = nums2;
-        int total = A.length + B.length;
-        int half = (total + 1) / 2;
-
-        if (B.length < A.length) {
-            int[] temp = A;
-            A = B;
-            B = temp;
+        if (nums1.length > nums2.length) { // Ensuring nums1 is always the smaller array
+            return findMedianSortedArrays(nums2, nums1);
         }
 
-        int l = 0;
-        int r = A.length;
-        while (l <= r) {
-            int i = (l + r) / 2;
-            int j = half - i;
+        int total = nums1.length + nums2.length;
+        int half = (total + 1) / 2;
+        int left = 0, right = nums1.length;
 
-            int Aleft = i > 0 ? A[i - 1] : Integer.MIN_VALUE;
-            int Aright = i < A.length ? A[i] : Integer.MAX_VALUE;
-            int Bleft = j > 0 ? B[j - 1] : Integer.MIN_VALUE;
-            int Bright = j < B.length ? B[j] : Integer.MAX_VALUE;
+        while (left <= right) {
+            int midA = (left + right) >> 1;
+            int midB = half - midA;
 
-            if (Aleft <= Bright && Bleft <= Aright) {
-                if (total % 2 != 0) {
-                    return Math.max(Aleft, Bleft);
+            int leftA = (midA > 0) ? nums1[midA - 1] : Integer.MIN_VALUE;
+            int leftB = (midB > 0) ? nums2[midB - 1] : Integer.MIN_VALUE;
+
+            int rightA = (midA < nums1.length) ? nums1[midA] : Integer.MAX_VALUE;
+            int rightB = (midB < nums2.length) ? nums2[midB] : Integer.MAX_VALUE;
+
+            if (leftA <= rightB && leftB <= rightA) {
+                if (total % 2 == 1) {
+                    return Math.max(leftA, leftB);
+                } else {
+                    return (Math.max(leftA, leftB) + Math.min(rightA, rightB)) / 2.0;
                 }
-                return (Math.max(Aleft, Bleft) + Math.min(Aright, Bright)) / 2.0;
-            } else if (Aleft > Bright) {
-                r = i - 1;
+            } else if (leftA > rightB) {
+                right = midA - 1;  // Fix: Decrease right boundary
             } else {
-                l = i + 1;
+                left = midA + 1;   // Fix: Increase left boundary
             }
         }
-        return -1;
+        return 0;
     }
 }
