2f21480992a1f22d50ca21d74d4c5bbc9c62f8a2
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-03-06 23:36:40 -0700

    Time: 102 ms (48.8%), Space: 50.3 MB (36%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
@@ -1,7 +1,11 @@
 class Solution {
     public int[] closestPrimes(int left, int right) {
         int[] result=new int[]{-1,-1};
-        ArrayList<Integer> primes= generatePrimesInRange(left, right);   
+
+        ArrayList<Integer> primes = generatePrimesInRangeUsingSieveOfEratosthenes(left, right);
+
+        // takes O(n log n)
+        // ArrayList<Integer> primes = generatePrimesInRange(left, right);   
         if(primes.size()<2){
             return result;
         }
@@ -16,6 +20,32 @@ class Solution {
         return result;     
 
     }
+
+    public ArrayList<Integer> generatePrimesInRangeUsingSieveOfEratosthenes(int left, int right){
+        ArrayList<Integer> primes=new ArrayList<>();
+        boolean[] isPrimes=new boolean[right+1];
+        Arrays.fill(isPrimes,true);
+        isPrimes[0]=false;
+        isPrimes[1]=false;
+        for(int num=0;num<=Math.sqrt(right);num++){
+            if(!isPrimes[num]) continue;
+            int j=num*2;
+            while(j<=(right)){
+                isPrimes[j]= false;
+                j+=num;
+            }
+        }
+
+        for(int num=left;num<=right;num++){
+            if(isPrimes[num])
+            primes.add(num);
+        }
+
+        return primes;
+    }
+
+
+// takes O(n log n)
     public ArrayList<Integer> generatePrimesInRange(int left, int right){
         ArrayList<Integer> primes=new ArrayList<>();
         for(int num=left;num<=right;num++){
