395eb9d87d3abe5bff5bd0e3e908f16b60cd54a1
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-03-10 04:50:28 -0600

    Time: 1948 ms (5.13%), Space: 50.3 MB (8.55%) - LeetHub

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
@@ -1,85 +1,41 @@
 class Solution {
-
-    public long countOfSubstrings(String word, int k) {
-        long numValidSubstrings = 0;
-        int start = 0;
-        int end = 0;
-        // keep track of counts of vowels and consonants
-        HashMap<Character, Integer> vowelCount = new HashMap<>();
-        int consonantCount = 0;
-
-        // compute index of next consonant for all indices
-        int[] nextConsonant = new int[word.length()];
-        int nextConsonantIndex = word.length();
-        for (int i = word.length() - 1; i >= 0; i--) {
-            nextConsonant[i] = nextConsonantIndex;
-            if (!isVowel(word.charAt(i))) {
-                nextConsonantIndex = i;
+    public boolean hasAllVowels(HashMap<Character, Integer> hMap){
+        ArrayList<Character> vowels = new ArrayList<>(List.of('a', 'e', 'i', 'o', 'u'));
+        for(Character vowel: vowels){
+            if(hMap.getOrDefault(vowel,0)==0){
+                return false;
             }
         }
-
-        // start sliding window
-        while (end < word.length()) {
-            // insert new letter
-            char newLetter = word.charAt(end);
-
-            // update counts
-            if (isVowel(newLetter)) {
-                vowelCount.put(
-                    newLetter,
-                    vowelCount.getOrDefault(newLetter, 0) + 1
-                );
-            } else {
-                consonantCount++;
-            }
-
-            // shrink window if too many consonants in our window
-            while (consonantCount > k) {
-                char startLetter = word.charAt(start);
-                if (isVowel(startLetter)) {
-                    vowelCount.put(
-                        startLetter,
-                        vowelCount.get(startLetter) - 1
-                    );
-                    if (vowelCount.get(startLetter) == 0) {
-                        vowelCount.remove(startLetter);
-                    }
-                } else {
-                    consonantCount--;
-                }
-                start++;
-            }
-
-            // while we have a valid window, try to shrink it
-            while (
-                start < word.length() &&
-                vowelCount.keySet().size() == 5 &&
-                consonantCount == k
-            ) {
-                // count the current valid substring, as well as valid substrings produced by appending more vowels
-                numValidSubstrings += nextConsonant[end] - end;
-                char startLetter = word.charAt(start);
-                if (isVowel(startLetter)) {
-                    vowelCount.put(
-                        startLetter,
-                        vowelCount.get(startLetter) - 1
-                    );
-                    if (vowelCount.get(startLetter) == 0) {
-                        vowelCount.remove(startLetter);
-                    }
-                } else {
-                    consonantCount--;
-                }
-
-                start++;
-            }
-            end++;
-        }
-
-        return numValidSubstrings;
+        return true;
     }
-
-    private boolean isVowel(char c) {
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    public long countOfSubstrings(String word, int k){
+        return this.countOfSubstringsGTEqK(word, k) - this.countOfSubstringsGTEqK(word, k+1);
+    }
+    public boolean isVowel(Character c){
+        return c.equals('a') || c.equals('e') || c.equals('i') || c.equals('o') || c.equals('u');
+    }
+    public long countOfSubstringsGTEqK(String word, int k) {
+        int left=0;
+        long count=0;
+        int non_vowel=0;
+        HashMap<Character, Integer> hMap=new HashMap<>();
+        for(int right=0;right<word.length();right++){
+            if(!this.isVowel(word.charAt(right))){
+                non_vowel+=1;
+            }else{
+                hMap.put(word.charAt(right),hMap.getOrDefault(word.charAt(right),0)+1);
+            }
+            while(this.hasAllVowels(hMap) && non_vowel>=k){
+                count+=(word.length()-right);
+                if(!this.isVowel(word.charAt(left))){
+                    non_vowel-=1;
+                }else{
+                    hMap.put(word.charAt(left),hMap.get(word.charAt(left))-1);
+                }
+                left+=1;
+                
+            }
+        }
+        return count;
     }
 }
