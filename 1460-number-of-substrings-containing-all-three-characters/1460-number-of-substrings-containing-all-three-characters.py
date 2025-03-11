f80397d08baa5fb773914caa52151283cb39e3f5
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-03-11 01:08:41 -0600

    Time: 138 ms (27.17%), Space: 17.9 MB (39.71%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
@@ -1,5 +1,5 @@
 class Solution:
-    def numberOfSubstrings(self, s: str) -> int:
+    def numberOfSubstrings1(self, s: str) -> int:
         char_last_seen_at_list=[-1,-1,-1]
         count_of_substrings=0
 
@@ -10,4 +10,24 @@ class Solution:
                 count_of_substrings+= (1 + min(char_last_seen_at_list[0],min(char_last_seen_at_list[1], char_last_seen_at_list[2])))
         
         return count_of_substrings
-        
+
+
+    def all_3_chars_at_least_k(self, char_counts, k):
+        return char_counts[0] >= k and char_counts[1] >= k and char_counts[2] >= k
+
+    def helper(self, s: str, k: int) -> int:
+        count_of_substrings = 0
+        char_counts = [0, 0, 0]  # counts for 'a', 'b', 'c'
+
+        left = 0
+        for right in range(len(s)):
+            char_counts[ord(s[right]) - ord('a')] += 1
+            while self.all_3_chars_at_least_k(char_counts, k):
+                count_of_substrings += (len(s) - right)
+                char_counts[ord(s[left]) - ord('a')] -= 1
+                left += 1
+
+        return count_of_substrings
+
+    def numberOfSubstrings(self, s: str) -> int:
+        return self.helper(s, 1)  # count substrings that have at least one 'a', 'b', 'c'
