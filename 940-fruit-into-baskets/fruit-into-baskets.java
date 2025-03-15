2e66195e96d93a8d1f5e648cf2e7f1c4fa5b2e8b
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-03-15 10:29:44 -0600

    Time: 49 ms (75.31%) | Memory: 55 MB (87.59%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -7,8 +7,8 @@ class Solution {
         for (int r=0;r<fruits.length;r++){
             hmap.put(fruits[r], hmap.getOrDefault(fruits[r],0)+1);
             while(hmap.size()>max_fruit_types){
-                hmap.put(fruits[l], hmap.getOrDefault(fruits[l],0)-1);
-                if(hmap.get(fruits[l])<=0){
+                hmap.put(fruits[l], hmap.get(fruits[l])-1);
+                if(hmap.get(fruits[l])==0){
                     hmap.remove(fruits[l]);
                 }
                 l++;
