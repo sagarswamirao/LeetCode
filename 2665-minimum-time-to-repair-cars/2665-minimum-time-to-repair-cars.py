83222c7edacacc759c91b8fb54c10a29499a9774
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-03-16 00:15:32 -0600

    Time: 725 ms (50.41%), Space: 21.8 MB (94.38%) - LeetHub

---
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.py
@@ -0,0 +1,23 @@
+class Solution:
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        
+        def can_service_all_car_in_given_time(ranks,time_limit,target):
+            num_of_cars_serviced=0
+            
+            for rank in ranks:
+                num_of_cars_serviced += floor(sqrt(time_limit / rank))
+            
+            return num_of_cars_serviced>=target
+        
+        low=0
+        high=100 * (cars**2)
+
+        while low<=high:
+            mid=(low+high)//2
+
+            if can_service_all_car_in_given_time(ranks, mid, cars):
+                high=mid-1
+            else:
+                low=mid+1
+        
+        return low
