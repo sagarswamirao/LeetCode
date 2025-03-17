e572a9769ca3fd9f573da43831487dcd427380e1
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-03-16 18:10:18 -0600

    Time: 6 ms (23.10%) | Memory: 18 MB (44.86%) - LeetSync

---
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        freq_dict={}
+        unpaired_count=0
+        for num in nums:
+            freq_dict[num]=freq_dict.get(num,0)+1
+            if freq_dict[num]%2==1:
+                unpaired_count+=1
+            else:
+                unpaired_count-=1
+        return unpaired_count==0
