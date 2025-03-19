f2227bc45f3ac1d272d61c45b6b51794d029fd76
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-03-18 22:35:42 -0600

    Time: 4 ms (88.93%), Space: 58.3 MB (48.02%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,21 +1,42 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int ans=-1;
-        //negative marking the value of indexes, and later reverting
-        for(int i=0;i<nums.length;i++){
-            //already marked once
-            if(nums[Math.abs(nums[i])]<0){
-                ans=Math.abs(nums[i]);
+        int n = nums.length;
+        int slow = 0;
+        int fast = 0;
+        while(true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow == fast){
                 break;
             }
-            nums[Math.abs(nums[i])]*=-1;
         }
-        for(int i=0;i<nums.length;i++){
-            nums[i]=Math.abs(nums[i]);
+        slow = 0;
+        while(nums[slow] != nums[fast]) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return ans;
-
+        return nums[slow];
     }
+
+
+    // public int findDuplicate(int[] nums) {
+    //     int ans=-1;
+    //     //negative marking the value of indexes, and later reverting
+    //     for(int i=0;i<nums.length;i++){
+    //         //already marked once
+    //         if(nums[Math.abs(nums[i])]<0){
+    //             ans=Math.abs(nums[i]);
+    //             break;
+    //         }
+    //         nums[Math.abs(nums[i])]*=-1;
+    //     }
+    //     for(int i=0;i<nums.length;i++){
+    //         nums[i]=Math.abs(nums[i]);
+    //     }
+    //     return ans;
+
+    // }
+
     // public int findDuplicate(int[] nums) {
     //     HashSet<Integer> hs=new HashSet<>();
     //     for(int num:nums){
