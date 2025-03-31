b23b0d7f5225ab6d48c8b5841f0e81595c739001
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-03-31 14:22:32 -0600

    Time: 21 ms (17.27%), Space: 35 MB (10.23%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,8 +1,5 @@
+from collections import Counter
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hset=set()
-        for num in nums:
-            if num in hset:
-                return True
-            hset.add(num)
-        return False
+        count_pairs=Counter(nums)
+        return True if max(count_pairs.values())>1 else False
