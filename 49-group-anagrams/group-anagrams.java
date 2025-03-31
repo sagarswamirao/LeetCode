b87d9044bca96289b923bf5d138ea2a2d54ecaf9
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-03-31 16:23:10 -0600

    Time: 18 ms (25.49%) | Memory: 48.7 MB (16.57%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,37 @@
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+        
+//         HashMap<String, List<String>> hashy = new HashMap<>();
+//         for(String s: strs){
+//             char[] arr = s.toCharArray();
+//             Arrays.sort(arr);
+//             String key = new String(arr);
+//             if(!hashy.containsKey(key)){
+//                 hashy.put(key, new ArrayList<String>());
+//             }
+//             hashy.get(key).add(s);
+//         }
+//         return new ArrayList<>(hashy.values());
+//     }
+// }
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) return new ArrayList<>();
+        Map<String, ArrayList> ans = new HashMap<String, ArrayList>();
+        int[] count = new int[26];
+        for (String s : strs) {
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray()) count[c - 'a']++;
+
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < 26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList<>());
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
+    }
+}
