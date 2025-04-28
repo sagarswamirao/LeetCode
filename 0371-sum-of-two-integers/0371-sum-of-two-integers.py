c98cca94515f2a1ce689742669fc1e22536f3190
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-04-27 19:36:15 -0600

    Time: 0 ms (100%), Space: 17.6 MB (80.13%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        # Mask to handle 32-bit integer overflow
+        MASK = 0xFFFFFFFF
+        INT_MAX = 0x7FFFFFFF   #because 31 bits only; 7 is 0111;
+
+        while b != 0:
+            # Carry is the AND of a and b
+            carry = (a & b) & MASK
+            # XOR gives the sum without carry
+            a = (a ^ b) & MASK
+            # Shift carry to the left
+            b = (carry << 1) & MASK
+
+        # Handle overflow for negative numbers
+        return a if a <= INT_MAX else ~(a ^ MASK)
