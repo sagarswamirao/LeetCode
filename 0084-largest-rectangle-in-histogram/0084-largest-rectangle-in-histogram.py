00878b93a3b1c55e2924b68746bb7e03e01af2d0
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-04-30 16:54:10 -0600

    Time: 171 ms (38.21%), Space: 35.5 MB (31.76%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -5,7 +5,7 @@ class Solution:
 
         for i, h in enumerate(heights):
             start=i
-            while stack and stack[-1][1] >= h:
+            while stack and stack[-1][1] > h:
                 index, height= stack.pop()
                 max_area=max(max_area, height * (i - index))
                 start=index
