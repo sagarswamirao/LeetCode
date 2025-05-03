07cbbe1ceca244f8f9bd6a3c57bc765966d775d0
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-05-02 18:19:31 -0600

    Time: 0 ms (100%), Space: 41.9 MB (44.88%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,14 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    // consider all 32 bits to reverse
+    public int reverseBits(int n) {
+        long answer = 0;
+        for (int i = 0; i < 32; i++) {
+            long bit = n & 1;
+            answer = answer ^ (bit << (31 - i));
+            n = n >> 1;
+        }
+        return (int) answer;
+    }
+
+}
