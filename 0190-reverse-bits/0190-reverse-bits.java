bee320a37302e7660ce7435a602535c980b9004b
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-05-02 18:20:59 -0600

    Time: 0 ms (100%), Space: 42 MB (33.57%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -5,7 +5,7 @@ public class Solution {
         long answer = 0;
         for (int i = 0; i < 32; i++) {
             long bit = n & 1;
-            answer = answer ^ (bit << (31 - i));
+            answer = answer | (bit << (31 - i));
             n = n >> 1;
         }
         return (int) answer;
