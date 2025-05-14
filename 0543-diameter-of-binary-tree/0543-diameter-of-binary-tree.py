a70a775903c59ce1b28ce37cf5fd1f5c6ebd89fa
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-05-14 01:32:13 -0600

    Time: 7 ms (46.79%), Space: 20.8 MB (82.46%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -6,17 +6,19 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        max_diameter=0
+        max_diameter = 0
+
+        # returns the height (not diameter)
         def recursive(node):
             nonlocal max_diameter
 
-            if node==None:
-                return -1
+            if not node:
+                return 0
 
-            left=recursive(node.left)
-            right=recursive(node.right)
-            max_diameter=max(max_diameter,left+right+2)
-            return max(left,right)+1
-        
+            left = recursive(node.left)
+            right = recursive(node.right)
+            max_diameter = max(max_diameter, left + right)
+            return max(left, right) + 1
+
         recursive(root)
         return max_diameter
