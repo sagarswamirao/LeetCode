3c861deaa59f2ba89c160c089b146f8b69287f68
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-05-17 22:50:10 -0600

    Time: 66 ms (24.93%), Space: 17.7 MB (71.05%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -8,21 +8,19 @@ class Solution:
         
         num1=num1[::-1]
         num2=num2[::-1]
+
         for idx1 in range(len(num1)):
             for idx2 in range(len(num2)):
                 ans_arr[idx1+idx2] += (int(num1[idx1]) * int(num2[idx2]))
-                ans_arr[idx1+idx2+1]+=ans_arr[idx1+idx2]//10
-                ans_arr[idx1+idx2]=ans_arr[idx1+idx2]%10
+                ans_arr[idx1+idx2+1]+=ans_arr[idx1+idx2]//10  #carry to next position
+                ans_arr[idx1+idx2]=ans_arr[idx1+idx2]%10 #sum after removing overflow
+
         ans_arr.reverse()
 
         start_idx=0
         for i in range(len(ans_arr)):
-            ans_arr[i]=str(ans_arr[i])
-
-        for i in range(len(ans_arr)):
-            if ans_arr[i]=='0':
+            if ans_arr[i]==0:
                 start_idx+=1
             else:
                 break
-
-        return ''.join(ans_arr[start_idx:])
+        return ''.join(map(str, ans_arr[start_idx:]))
