6dbbf9c60bd24d5cfaa3a2d95ff5dcb56f074b6a
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-05-18 16:30:12 -0600

    Time: 0 ms (100%), Space: 17.9 MB (65.18%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,20 +1,21 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ds=[]
-        ans=[]
-        def recursive(nums, i, n, ds, ans):
-            #using pick/not-pick
-            if i==n:
-                ans.append(list(ds))
-            
-            if i<n:
-                #not-pick
-                recursive(nums, i+1, n, ds, ans)
+        ds = []
+        ans = []
 
-                #pick
-                ds.append(nums[i])
-                recursive(nums, i+1, n, ds, ans)
-                ds.pop()
+        def recursive(nums, i, n, ds, ans):
+            # using pick/not-pick
+            if i == n:
+                ans.append(list(ds))
+                return
+
+            # pick
+            ds.append(nums[i])
+            recursive(nums, i + 1, n, ds, ans)
+            ds.pop()
+
+            # not-pick
+            recursive(nums, i + 1, n, ds, ans)
 
         recursive(nums, 0, len(nums), ds, ans)
         return ans
