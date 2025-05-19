439be40f08757d5eb5b470299401d48668749097
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-05-18 20:37:13 -0600

    Time: 7 ms (82.91%), Space: 18 MB (39.87%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -9,10 +9,17 @@ class Solution:
                 if curr_i>=n or target<0:
                     return
                 
+                # Optimal: For Loop
                 for next_i in range(curr_i,n):
                     ds.append(candidates[next_i])
                     recursive(candidates, target-candidates[next_i], next_i, n, ds, ans)
                     ds.pop()
 
+                #Better: Pick Unpick
+                # ds.append(candidates[curr_i])
+                # recursive(candidates, target-candidates[curr_i], curr_i, n, ds, ans)
+                # ds.pop()
+                # recursive(candidates, target, curr_i+1, n, ds, ans)
+                
             recursive(candidates, target, 0, n, [], ans)
             return ans
