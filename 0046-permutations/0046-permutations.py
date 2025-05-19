e0be180a1819c55e8d82ed7481907f8c364bb603
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-05-18 23:23:21 -0600

    Time: 0 ms (100%), Space: 18.2 MB (14.89%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,24 +1,22 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        in_use=[False for i in range(len(nums))]
-        answer=[]
+        in_use = [False for i in range(len(nums))]
+        answer = []
 
         def recursive(n, ds):
-            if len(ds)==n:
+            if len(ds) == n:
                 answer.append(ds.copy())
                 return
-            
+
             for next_i in range(len(in_use)):
-                if in_use[next_i]==True:
+                if in_use[next_i] is True:
                     continue
-                in_use[next_i]=True
+                in_use[next_i] = True
                 ds.append(nums[next_i])
                 recursive(n, ds)
-                in_use[next_i]=False
+                in_use[next_i] = False
                 ds.pop()
-            
-            
-        
+
         recursive(len(nums), [])
 
         return answer
