4530051beafa9c465da75f0cd959d4600b83acde
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-05-18 23:22:58 -0600

    Time: 0 ms (100%), Space: 18 MB (33.35%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -3,23 +3,22 @@ class Solution:
         in_use=[False for i in range(len(nums))]
         answer=[]
 
-        def recursive(i, n, ds):
+        def recursive(n, ds):
             if len(ds)==n:
                 answer.append(ds.copy())
                 return
-            # if i>=n:
-            #     return
             
             for next_i in range(len(in_use)):
-                if in_use[next_i]==False:
-                    in_use[next_i]=True
-                    ds.append(nums[next_i])
-                    recursive(next_i, n, ds)
-                    in_use[next_i]=False
-                    ds.pop()
+                if in_use[next_i]==True:
+                    continue
+                in_use[next_i]=True
+                ds.append(nums[next_i])
+                recursive(n, ds)
+                in_use[next_i]=False
+                ds.pop()
             
             
         
-        recursive(0, len(nums), [])
+        recursive(len(nums), [])
 
         return answer
