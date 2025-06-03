518097ab10de477805b22fbd6ca0f4cb49d623d8
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-06-02 19:30:21 -0600

    Time: 59 ms (96.35%), Space: 51.4 MB (21.96%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -5,8 +5,7 @@ class Solution:
         prev_max_end_time = -inf
         
         for curr_start, curr_end in intervals:
-            if curr_start < prev_max_end_time:
+            if curr_start >= prev_max_end_time:
                 ans+=1
-            else:
                 prev_max_end_time = curr_end
-        return ans
+        return len(intervals)-ans
