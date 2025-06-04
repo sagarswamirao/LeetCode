9ffef210435b3b4a366c6cc17a0773f02a89dcad
sagarswamirao <47278626+sagarswamirao@users.noreply.github.com>
2025-06-04 01:48:16 -0600

    Time: 57 ms (13.46%), Space: 130.2 MB (5.51%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,15 +1,15 @@
 class LRUCache {
-    class DoublyListNode{
+    class DoublyListNode {
         int key;
         int val;
         DoublyListNode prev;
         DoublyListNode next;
-        
-        DoublyListNode(int key, int val){
-            this.key=key;
-            this.val=val;
-            this.prev=null;
-            this.next=null;
+
+        DoublyListNode(int key, int val) {
+            this.key = key;
+            this.val = val;
+            this.prev = null;
+            this.next = null;
         }
     }
 
@@ -17,54 +17,66 @@ class LRUCache {
     private DoublyListNode head, tail;
     // Insert at tail, remove from head
     HashMap<Integer, DoublyListNode> hMap;
+
     public LRUCache(int capacity) {
-        this.capacity=capacity;
-        this.head=new DoublyListNode(-1,-1);
-        this.tail=new DoublyListNode(-1,-1);
-        this.tail.prev=this.head;
-        this.head.next=this.tail;
-        this.hMap=new HashMap<>();
+        this.capacity = capacity;
+        this.head = new DoublyListNode(-1, -1);
+        this.tail = new DoublyListNode(-1, -1);
+        this.tail.prev = this.head;
+        this.head.next = this.tail;
+        this.hMap = new HashMap<>();
     }
-    
+
     public int get(int key) {
-        if(this.hMap.containsKey(key)){
+        if (this.hMap.containsKey(key)) {
             this.moveToEnd(key);
             return this.hMap.get(key).val;
         }
         return -1;
     }
-    
-    private void moveToEnd(int key){
-        DoublyListNode tmp=this.removeFromCache(key);
-        this.addToCache(tmp.key, tmp.val);
-    }
-    private DoublyListNode removeFromCache(int key){
-            DoublyListNode nodeToRemove=this.hMap.get(key);
-            nodeToRemove.prev.next=nodeToRemove.next;
-            nodeToRemove.next.prev=nodeToRemove.prev;
-            this.hMap.remove(key);
-            return nodeToRemove;
+
+private void moveToEnd(int key) {
+    DoublyListNode node=this.hMap.get(key);
+    // Detach
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+
+    // Attach to tail
+    node.prev = tail.prev;
+    node.next = tail;
+    tail.prev.next = node;
+    tail.prev = node;
+}
+
+
+    private DoublyListNode removeFromCache(int key) {
+        DoublyListNode nodeToRemove = this.hMap.get(key);
+        nodeToRemove.prev.next = nodeToRemove.next;
+        nodeToRemove.next.prev = nodeToRemove.prev;
+        this.hMap.remove(key);
+        return nodeToRemove;
     }
 
-    private void addToCache(int key, int value){
-            DoublyListNode nodeToAdd=new DoublyListNode(key, value);
-            nodeToAdd.next=this.tail;
-            nodeToAdd.prev=this.tail.prev;
-            nodeToAdd.prev.next=nodeToAdd;
-            this.tail.prev=nodeToAdd;
-            this.hMap.put(key, nodeToAdd);
+    private void addToCache(int key, int value) {
+        DoublyListNode nodeToAdd = new DoublyListNode(key, value);
+        nodeToAdd.next = this.tail;
+        nodeToAdd.prev = this.tail.prev;
+        nodeToAdd.prev.next = nodeToAdd;
+        this.tail.prev = nodeToAdd;
+        this.hMap.put(key, nodeToAdd);
     }
+
     public void put(int key, int value) {
-        if(hMap.containsKey(key)){
+        if (hMap.containsKey(key)) {
             //remove
             this.removeFromCache(key);
         }
         //add
-        this.addToCache(key,value);
+        this.addToCache(key, value);
         //if max. remove from head
-            if(this.hMap.size()>capacity){
+        if (this.hMap.size() > capacity) {
             this.removeFromCache(this.head.next.key);
-            }
+        }
     }
 }
 
